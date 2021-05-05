c9f7b75b5d48dde51d7d67e8225333d0a99bc570
Vladislav Voloshin <voloshinw@gmail.com>
2021-05-05 17:36:51 +0300

    added beta-file for schedule handler and added logic for sending this file

--- handlers/users/schedule_handler.py
+++ handlers/users/schedule_handler.py
@@ -1,18 +1,19 @@
 import datetime
 
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, InputFile
 
 from keyboards.inline.callback_data_schedule import day_callback
-from keyboards.inline.schedule import schedule
-from loader import dp
+from loader import dp, bot
 
 
 # Обработчик инлайн кнопки первого дня в блоке расписание
 @dp.callback_query_handler(day_callback.filter(day='first'))
 async def day_one(call: CallbackQuery):
+    photo_bytes = InputFile(path_or_bytesio='static/карта-базы.png')
     if datetime.date.today() >= datetime.date(2021, 5, 19):
         await call.answer(cache_time=60)
-        await call.message.answer('Расписание')
+        await call.message.answer('Присылаю расписание на первый день')
+        await bot.send_photo(chat_id=call.from_user.id, photo=photo_bytes)
     else:
         await call.answer(cache_time=60)
         await call.message.answer(text='Прости, расписание еще недоступно')
@@ -21,9 +22,11 @@ async def day_one(call: CallbackQuery):
 # Обработчик инлайн кнопки второго дня в блоке расписание
 @dp.callback_query_handler(day_callback.filter(day='second'))
 async def day_two(call: CallbackQuery):
+    photo_bytes = InputFile(path_or_bytesio='static/карта-базы.png')
     if datetime.date.today() >= datetime.date(2021, 5, 20):
         await call.answer(cache_time=60)
-        await call.message.answer('Расписание')
+        await call.message.answer('Присылаю расписание на второй день')
+        await bot.send_photo(chat_id=call.from_user.id, photo=photo_bytes)
     else:
         await call.answer(cache_time=60)
         await call.message.answer(text='Прости, расписание еще недоступно')
@@ -32,9 +35,11 @@ async def day_two(call: CallbackQuery):
 # Обработчик инлайн кнопки третьего дня в блоке расписание
 @dp.callback_query_handler(day_callback.filter(day='third'))
 async def day_three(call: CallbackQuery):
+    photo_bytes = InputFile(path_or_bytesio='static/карта-базы.png')
     if datetime.date.today() >= datetime.date(2021, 5, 21):
         await call.answer(cache_time=60)
-        await call.message.answer('Расписание')
+        await call.message.answer('Присылаю расписание на третий день')
+        await bot.send_photo(chat_id=call.from_user.id, photo=photo_bytes)
     else:
         await call.answer(cache_time=60)
         await call.message.answer(text='Прости, расписание еще недоступно')
@@ -43,9 +48,11 @@ async def day_three(call: CallbackQuery):
 # Обработчик инлайн кнопки четвертого дня в блоке расписание
 @dp.callback_query_handler(day_callback.filter(day='fourth'))
 async def day_fourth(call: CallbackQuery):
+    photo_bytes = InputFile(path_or_bytesio='static/карта-базы.png')
     if datetime.date.today() >= datetime.date(2021, 5, 21):
         await call.answer(cache_time=60)
-        await call.message.answer('Расписание')
+        await call.message.answer('Присылаю расписание на четвертый день')
+        await bot.send_photo(chat_id=call.from_user.id, photo=photo_bytes)
     else:
         await call.answer(cache_time=60)
         await call.message.answer(text='Прости, расписание еще недоступно')
