05973ebeb2fb4d7ff856a084efcffd37f628cab4
Vladislav Voloshin <voloshinw@gmail.com>
2021-05-12 19:21:41 +0300

    added return with main keyboard after command /start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
+from keyboards.default import menu
 from loader import dp
 
 
@@ -10,4 +11,5 @@ async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}!\n"
                          f"Я - чатбот VIII Школы Актива РЭУ им. Г.В.Плеханова!\n"
                          f"У меня ты сможешь узнать всю необходимую информацию, касаемо Школы Актива!")
+    await message.answer('Выберите нужную кнопку', reply_markup=menu)
 
